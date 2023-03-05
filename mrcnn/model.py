391a93d5200dfe2fbc1d8b8bd2210b83a1d75017
Ture-Dukale <86917357+Ture-Dukale@users.noreply.github.com>
2023-03-05 19:04:29 +0300

    Update model.py
    sess = tf.Session(config=config) -> tf.compat.v1.Session() #(model line 39)

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -36,7 +36,7 @@ from keras.backend import set_session
 import tensorflow as tf
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
-sess = tf.Session(config=config)
+sess = tf.compat.v1.Session(config=config)
 set_session(sess)  # set this TensorFlow session as the default session for Keras
 
 ############################################################
