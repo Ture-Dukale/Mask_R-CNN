d34ff8c86a811b6bf4bcf38ec425516805c1b513
Ture-Dukale <86917357+Ture-Dukale@users.noreply.github.com>
2023-03-11 17:58:31 +0300

    Update parse_args.py

--- mrcnn/parse_args.py
+++ mrcnn/parse_args.py
@@ -7,13 +7,13 @@ def parse_args():
     :return: Populated namespace.
     """
     parser = argparse.ArgumentParser(description='baseline Mask R-CNN')
-    parser.add_argument('/kaggle/input/train-val-imgs/data', #required=True,
-                        metavar='path/to/dataset',
+    parser.add_argument('--dataset', required=True,
+                        metavar='/kaggle/input/train-val-imgs/data',
                         help='Directory of the dataset')
     parser.add_argument('--continue_train', type=str, required=False, default='None',
                         metavar="/path/to/latest/weights.h5", help="Path to lastest training weights .h5 file")
-    parser.add_argument('/kaggle/working/Mask_R-CNN/mobile_mask_rcnn_coco.h5', #required=False,
-                        metavar='/path/to/pretrained/weight.h5', help="Path to trained weight")
+    parser.add_argument('--weight', #required=False,
+                        metavar='/kaggle/working/Mask_R-CNN/mobile_mask_rcnn_coco.h5', help="Path to trained weight")
     parser.add_argument('--image', required=False,
                         metavar='/path/to/testing/image/directory', help="Path to testing image directory")
     parser.add_argument('--video', required=False,
