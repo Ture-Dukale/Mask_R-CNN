d334b734e20c35ee62c4502ed4aaf268b4138d12
Ture-Dukale <86917357+Ture-Dukale@users.noreply.github.com>
2023-03-11 17:53:32 +0300

    Update parse_args.py

--- mrcnn/parse_args.py
+++ mrcnn/parse_args.py
@@ -7,12 +7,12 @@ def parse_args():
     :return: Populated namespace.
     """
     parser = argparse.ArgumentParser(description='baseline Mask R-CNN')
-    parser.add_argument('/kaggle/input/train-val-imgs/data', required=True,
+    parser.add_argument('/kaggle/input/train-val-imgs/data', #required=True,
                         metavar='path/to/dataset',
                         help='Directory of the dataset')
     parser.add_argument('--continue_train', type=str, required=False, default='None',
                         metavar="/path/to/latest/weights.h5", help="Path to lastest training weights .h5 file")
-    parser.add_argument('/kaggle/working/Mask_R-CNN/mobile_mask_rcnn_coco.h5', required=False,
+    parser.add_argument('/kaggle/working/Mask_R-CNN/mobile_mask_rcnn_coco.h5', #required=False,
                         metavar='/path/to/pretrained/weight.h5', help="Path to trained weight")
     parser.add_argument('--image', required=False,
                         metavar='/path/to/testing/image/directory', help="Path to testing image directory")
